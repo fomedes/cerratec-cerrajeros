68f72264d1f809172b5b4567d2a968ef03768728
[Francisco Omedes] <omedesf@gmail.com>
2026-02-28 16:12:05 +0000

    siteConfig update

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -1,29 +1,29 @@
 import type { SocialIconKey } from "../icons/socialIcons";
 
 export const siteConfig = {
-  name: "CerraNova Cerrajeros 24h",
+  name: "Cerrajero Camp",
   phone: {
-    raw: "+34687569885",
-    formatted: "687 56 98 85",
-    whatsapp: "34687569885",
+    raw: "+34632862468",
+    formatted: "632 86 24 68",
+    whatsapp: "34632862468",
   },
-  email: "info@cerranovacerrajeros.com",
+  email: "info@cerrajerocamp.com",
 
   social: [
     {
       key: "facebook" as SocialIconKey,
       label: "Facebook",
-      url: "https://www.facebook.com/cerratectarragona",
+      url: "https://www.facebook.com/cerrajerocamp",
     },
     {
       key: "instagram" as SocialIconKey,
       label: "Instagram",
-      url: "https://www.instagram.com/cerratectarragona",
+      url: "https://www.instagram.com/cerrajerocamp",
     },
     {
       key: "x" as SocialIconKey,
       label: "X",
-      url: "https://x.com/cerratec24h",
+      url: "https://x.com/cerrajerocamp",
     },
   ],
 };
